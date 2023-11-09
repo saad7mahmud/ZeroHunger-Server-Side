51c2a0b1213f7ff3984db6adaeed1646ece06537
saad7mahmud <saad7mahmud@gmail.com>
2023-11-09 13:49:04 +0600

    server side finalised

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     // Create Collections
     const availableFoodCollection = client
@@ -226,7 +226,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
